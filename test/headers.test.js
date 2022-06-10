e4e5de3ae25ad36d574e7308e5fb646c027d343d
TamilarasuM <40888141+TamilarasuM@users.noreply.github.com>
2022-06-10 14:30:47 +0530

    header test case added

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,11 +1,39 @@
 import { localize } from '@lion/localize';
 import '../../locale/inline-data.js';
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/header/Header.js';
 
 
 
-describe('loan-header', () => {
- 
+describe('loan-header component check', () => {
+    it('checks header title', async () => {
+        const el = await fixture(html` <loan-header></loan-header> `);
+        const headerTxt = await el.shadowRoot.querySelector('p').innerText;
+        console.log(el.shadowRoot.querySelector('p').innerText);
+        expect(headerTxt).to.equal('Apply Loan');
+    });
+    it('passes accessibility test For EN (validated by Class atrribute)', async () => {
+        const el = await fixture(html` <loan-header></loan-header> `);
+        const enBtn = await el.shadowRoot.querySelector('button[class="en-GB bg-btn-color "]');
+        await expect(enBtn).to.be.accessible();
+    });
+    it('passes accessibility test For NL - (validated by ID atrribute)', async () => {
+        const el = await fixture(html` <loan-header></loan-header> `);
+        const enBtn = await el.shadowRoot.querySelector('button[id="nl-NL"]');
+        await expect(enBtn).to.be.accessible();
+    });
+    it('passes accessibility test For language BTN click count', async () => {
+        const el = await fixture(html` <loan-header></loan-header> `);
+        const enBtn = await el.shadowRoot.querySelector('button[id="nl-NL"]');
+        const myFunctionStub = stub(el, 'localeChanged');
+        enBtn.click();
+        expect(myFunctionStub).to.have.callCount(1);
+    });
+    it('passes accessibility test For language BTN click', async () => {
+        const el = await fixture(html` <loan-header></loan-header> `);
+        const enBtn = await el.shadowRoot.querySelector('button[id="en-GB"]');
+        await enBtn.click();
+        await expect(localize.locale).to.equal('en-GB');
+    });
 });
